7f9b000bacf8d1b5d55f27dc05ea1d443a348785
JensBaumannDev <baumannjens@outlook.de>
2026-02-11 19:51:29 +0100

    added handleCellClick function - you can now place cross and circle one by one

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let fields = [null, "circle", null, null, "cross", null, null];
+let fields = [null, null, null, null, null, null, null, null, null];
 
 function init() {
   render();
@@ -17,9 +17,24 @@ function generateCrossSVG() {
   return `
     <svg class="cross-svg" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
       <line class="cross-line cross-line-first" x1="17" y1="17" x2="53" y2="53"></line>
-      <line class="cross-line cross-line-second" x1="53" y1="17" x2="17" y2="53"></line>
+          <line class="cross-line cross-line-second" x1="53" y1="17" x2="17" y2="53"></line>
     </svg>
   `;
+}
+
+function handleCellClick(td, index) {
+  if (fields[index] !== null) {
+    td.removeAttribute("onclick");
+    return;
+  }
+
+  const movesPlayed = fields.filter((v) => v !== null).length;
+  const shape = movesPlayed % 2 === 0 ? "circle" : "cross";
+
+  fields[index] = shape;
+  td.innerHTML = shape === "circle" ? generateCircleSVG() : generateCrossSVG();
+
+  td.removeAttribute("onclick");
 }
 
 function render() {
@@ -28,7 +43,6 @@ function render() {
 
   for (let i = 0; i < 3; i++) {
     html += "<tr>";
-
     for (let col = 0; col < 3; col++) {
       const index = i * 3 + col;
       let symbol = "";
@@ -39,9 +53,12 @@ function render() {
         symbol = generateCrossSVG();
       }
 
-      html += `<td>${symbol}</td>`;
+      if (fields[index] === null) {
+        html += `<td onclick="handleCellClick(this, ${index})"></td>`;
+      } else {
+        html += `<td>${symbol}</td>`;
+      }
     }
-
     html += "</tr>";
   }
 
